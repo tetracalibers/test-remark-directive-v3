cdb21e18fdec8fe25fe312347c2f55d3085f07ea
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-17 12:16:29 +0900

    Astroなしでは正常に動くのか実験

---
+++ vanilla/example.js
@@ -0,0 +1,50 @@
+/// <reference types="mdast-util-to-hast" />
+/// <reference types="mdast-util-directive" />
+
+import { h } from 'hastscript'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import remarkDirective from 'remark-directive'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { read } from 'to-vfile'
+import { unified } from 'unified'
+import { visit } from 'unist-util-visit'
+
+const file = await unified()
+  .use(remarkParse)
+  .use(remarkDirective)
+  .use(myRemarkPlugin)
+  .use(remarkRehype)
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(await read('example.md'))
+
+console.log(String(file))
+
+// This plugin is an example to let users write HTML with directives.
+// It’s informative but rather useless.
+// See below for others examples.
+function myRemarkPlugin() {
+  /**
+   * @param {import('mdast').Root} tree
+   *   Tree.
+   * @returns {undefined}
+   *   Nothing.
+   */
+  return function (tree) {
+    visit(tree, function (node) {
+      if (
+        node.type === 'containerDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'textDirective'
+      ) {
+        const data = node.data || (node.data = {})
+        const hast = h(node.name, node.attributes || {})
+
+        data.hName = hast.tagName
+        data.hProperties = hast.properties
+      }
+    })
+  }
+}
---
+++ vanilla/example.md
@@ -0,0 +1,10 @@
+:::main{#readme}
+
+Lorem:br
+ipsum.
+
+::hr{.red}
+
+A :i[lovely] language know as :abbr[HTML]{title="HyperText Markup Language"}.
+
+:::
